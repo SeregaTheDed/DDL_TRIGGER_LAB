8079217303d7f274937f4bdd92a4ff62c1c01497
s.shtanko <s.shtanko@krista.ru>
2023-12-03 03:25:30 +0300

    Добавьте файлы проекта.

---
+++ DDL_TRIGGER_LAB/Trigger_PrintTableNameOnInsert.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.SqlServer.Server;
+
+namespace DDL_TRIGGER_LAB
+{
+    public class Trigger_PrintTableNameOnInsert
+    {
+        [SqlTrigger(Name = "PrintTableNameOnInsert", Event = "FOR INSERT")]
+        public static void PrintTableNameOnInsert()
+        {
+            SqlPipe pipe = SqlContext.Pipe;
+            pipe.Send("Я добрый CRL триггер. Я рад, что меня вызвали ^_^");
+        }
+    }
+}
